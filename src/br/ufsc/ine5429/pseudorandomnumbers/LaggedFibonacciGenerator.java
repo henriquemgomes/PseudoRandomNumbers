e0101a30d5640ed0a48ed97653fb3c00c7eb9153
henriquemgomes <henriquemcgomess@gmail.com>
2021-12-10 23:56:24 -0300

    REF removed needless loop from lagged fibonacci

--- src/br/ufsc/ine5429/pseudorandomnumbers/LaggedFibonacciGenerator.java
+++ src/br/ufsc/ine5429/pseudorandomnumbers/LaggedFibonacciGenerator.java
@@ -7,27 +7,24 @@ public class LaggedFibonacciGenerator {
     public static BigInteger generatePseudoRandomNumber(int size, int firstIndex, int secondIndex) {
         int j = firstIndex;
         int k = secondIndex; 
-        int iterations = size - 2;
-        int[] randomNumberDigits = new int[iterations];
+        int iterations = size - 1;
         BigInteger seed = BigInteger.valueOf(System.nanoTime());
-        //BigInteger seed = new BigInteger("3643825324300600");
         int[] seedArray = new int[seed.toString().length()];
+        String randomBitString = "1";
         for (int i = 0; i < seed.toString().length(); i++) {
-          // - '0' para pegar a diferença do código do caractere
+          // - '0' para pegar a diferença do código ascii do caractere
           seedArray[i] = seed.toString().charAt(i) - '0';
         }
         for (int i = 0; i < iterations; i++) {
-            //Utils.printArray(seedArray);
-            //System.out.print(" --algarismo-calculado--> ");
             int newPseudoRandom = (seedArray[j-1] + seedArray[k-1]) % 2;
-            //System.out.println(newPseudoRandom);
             for (int l = 0; l < seedArray.length - 1; l++) {
                 seedArray[l] = seedArray[l+1];
             }
             seedArray[seedArray.length - 1] = newPseudoRandom;
-            randomNumberDigits[i] = newPseudoRandom;
+            randomBitString += newPseudoRandom;
         }
-        BigInteger pseudoRandomNumber = new BigInteger("1"+Utils.transformArrayIntoString(randomNumberDigits), 2);
+
+        BigInteger pseudoRandomNumber = new BigInteger(randomBitString, 2);
         return pseudoRandomNumber;
     }
 }
