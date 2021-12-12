ecdfd4fea331155bfe0ba55de8e1415f806da3eb
henriquemgomes <henriquemcgomess@gmail.com>
2021-12-12 20:20:58 -0300

    REF miller rabin

--- src/br/ufsc/ine5429/pseudorandomnumbers/MillerRabinPrimalityTest.java
+++ src/br/ufsc/ine5429/pseudorandomnumbers/MillerRabinPrimalityTest.java
@@ -1,68 +1,47 @@
 package br.ufsc.ine5429.pseudorandomnumbers;
 
 import java.math.BigInteger;
+import java.util.Random;
 
 public class MillerRabinPrimalityTest {
-
-    public static boolean isPrime(BigInteger n){
-        System.out.println("testando: "+ n);
-        if(n.mod(new BigInteger("2")).equals(new BigInteger("0"))){
-            return false;
-        }
-
-        int k = 1;
-        BigInteger m = null;
-        BigInteger nMinOne = n.subtract(new BigInteger("1"));
-        BigInteger a = null;
-        BigInteger b = null;
-        BigInteger[] lastDivided = new BigInteger[2];
-
-        while (true) {
-            lastDivided = nMinOne.divideAndRemainder(new BigInteger("2").pow(k));
-            System.out.println("prob: "+ lastDivided[0]+", "+ lastDivided[1]);
-            if (lastDivided[1].equals(new BigInteger("0"))) {
-                m = lastDivided[0];
-                k++;
-            }else if(k == 1 && !lastDivided[1].equals(new BigInteger("0"))){
-               // System.out.println("caiu no if matador");
+    public boolean isPrime(BigInteger n){
+        for (int i = 0; i < 40; i++) {
+            if(!testPrime(n)){
                 return false;
-            }else {
-                k = k-1;
-                break;
             }
         }
-       // System.out.println();
-        
-        while (true) {
-            a = LinearCongruentialGenerator.generatePseudoRandomNumber(nMinOne.bitLength() - 1, 5, 2, 257, true);
-            if (a.compareTo(new BigInteger("1")) == 1 && a.compareTo(nMinOne) == -1) {
-                break;
-            }
+        return true;
+    }
+
+    private boolean testPrime(BigInteger n){
+        BigInteger nMinOne = n.subtract(BigInteger.ONE);
+        BigInteger base = nMinOne;
+        BigInteger a = BigInteger.ZERO;
+
+        do {
+            a = new BigInteger(nMinOne.bitLength()-1, new Random());
+        } while (a.compareTo(nMinOne) >= 0 || a.compareTo(BigInteger.ONE) <= 0);
+
+        int k = 0;
+        while (base.mod(BigInteger.TWO).equals(BigInteger.ZERO)) {
+            base = base.shiftRight(1);
+            k++;
         }
 
-        int i = 1;
-        System.out.println("a="+a+" m="+m+" n="+n);
-        b = a.modPow(m, n);
-        System.out.println("b: "+ b);
-        BigInteger lastB = null;
-        int repetitions = 0;
-        while (true) {
+        BigInteger currentVal = a.modPow(base,n);
 
-            if(b.equals(new BigInteger("1")) || repetitions>=10){
-                return false;
-            }else if(b.equals(nMinOne)){
+        if(currentVal.equals(BigInteger.ONE)){
+            return true;
+        }
+
+        for (int i = 0; i < k; i++) {
+            if(currentVal.equals(n.subtract(BigInteger.ONE))){
                 return true;
-            }
-           // System.out.println(b);
-            i++;
-            lastB = b;
-            b = b.pow(i).mod(n);
-            if(lastB.equals(b)){
-               // System.out.println(b);
-                repetitions++;
             }else{
-                repetitions = 0;
+                currentVal = currentVal.modPow(BigInteger.TWO, n);
             }
-        }        
+        }
+        
+        return false;
     }
 }
